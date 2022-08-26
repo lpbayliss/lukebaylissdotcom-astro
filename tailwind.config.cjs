98ca5b7acec208ad2895f90d5ac142eab31eef8a
Luke Bayliss <lukepbayliss@gmail.com>
2022-08-26 15:55:02 +1000

    Styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        canvas: {
+          light: "#fffffe",
+          DEFAULT: "#fffffe",
+          dark: "#16161a",
+        },
+        headline: {
+          light: "#2b2c34",
+          DEFAULT: "#2b2c34",
+          dark: "#fffffe",
+        },
+        paragraph: {
+          light: "#2b2c34",
+          DEFAULT: "#2b2c34",
+          dark: "#94a1b2",
+        },
+        primary: {
+          light: "#2b2c34",
+          DEFAULT: "#2b2c34",
+          dark: "#94a1b2",
+        },
+        surface: {
+          light: "#d1d1e9",
+          DEFAULT: "#d1d1e9",
+          dark: "#72757e",
+        },
+      },
+    },
   },
   plugins: [],
 };
