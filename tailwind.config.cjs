4dcdcd3f9f4f4a7e98fee9aa9734e323993e3c43
Luke Bayliss <lukepbayliss@gmail.com>
2022-08-26 17:40:29 +1000

    more stuff

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,31 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        canvas: {
-          light: "#fffffe",
-          DEFAULT: "#fffffe",
-          dark: "#16161a",
-        },
-        headline: {
-          light: "#2b2c34",
-          DEFAULT: "#2b2c34",
-          dark: "#fffffe",
-        },
-        paragraph: {
-          light: "#2b2c34",
-          DEFAULT: "#2b2c34",
-          dark: "#94a1b2",
-        },
-        primary: {
-          light: "#2b2c34",
-          DEFAULT: "#2b2c34",
-          dark: "#94a1b2",
-        },
-        surface: {
-          light: "#d1d1e9",
-          DEFAULT: "#d1d1e9",
-          dark: "#72757e",
-        },
+        canvas: "#16161a",
+        headline: "#fffffe",
+        paragraph: "#94a1b2",
+        primary: "#7f5af0",
+        surface: "#72757e",
       },
     },
   },
